39784b7cffd87e11f89934180cfec15091d1d477
Matias Senger <m.senger@hotmail.com>
2021-03-15 20:01:50 +0100

    First implementation of calibration correction

--- psi_test_setup/DigitalTDCTestSetup.py
+++ psi_test_setup/DigitalTDCTestSetup.py
@@ -1,6 +1,9 @@
 from .FPGA import FPGA
 from .DAC import DAC
 import time
+import pandas
+from scipy import interpolate
+import numpy as np
 
 _COMMANDS = {
 	# See https://drive.switch.ch/index.php/s/u4TGjVyMPjTvswf in /Adapter_TPIX/FPGA/doc/spi2.txt
@@ -75,3 +78,73 @@ class _DigitalTDCTestSetup:
 		time.sleep(10e-3)
 		self._fpga.send_and_receive(_COMMANDS['SeqInit'])
 		time.sleep(10e-3)
+	
+	def load_calibration_files(self, D_calibration_file: str, FTUNE_calibration_file: str):
+		D_data = pandas.read_csv(
+			D_calibration_file,
+			delimiter = ',',
+			comment = '#',
+		)
+		self.time_to_D = {}
+		for delay_chip in sorted(set(D_data['delay_chip'])):
+			self.time_to_D[delay_chip] = interpolate.interp1d(
+				x = D_data.loc[D_data['delay_chip']==delay_chip, 'average delay (s)'],
+				y = D_data.loc[D_data['delay_chip']==delay_chip, 'D'],
+			)
+		FTUNE_data = pandas.read_csv(
+			FTUNE_calibration_file,
+			delimiter = ',',
+			comment = '#',
+		)
+		self.time_to_FTUNE = {}
+		self.FTUNE_to_time = {}
+		for delay_chip in sorted(set(D_data['delay_chip'])):
+			self.time_to_FTUNE[delay_chip] = interpolate.interp1d(
+				x = FTUNE_data.loc[FTUNE_data['delay_chip']==delay_chip, 'average delay (s)'],
+				y = FTUNE_data.loc[FTUNE_data['delay_chip']==delay_chip, 'FTUNE (V)'],
+			)
+			self.FTUNE_to_time[delay_chip] = interpolate.interp1d(
+				y = FTUNE_data.loc[FTUNE_data['delay_chip']==delay_chip, 'average delay (s)'],
+				x = FTUNE_data.loc[FTUNE_data['delay_chip']==delay_chip, 'FTUNE (V)'],
+			)
+		
+		self.D_calibration_data = {}
+		for delay_chip in sorted(set(D_data['delay_chip'])):
+			self.D_calibration_data[delay_chip] = {}
+			self.D_calibration_data[delay_chip]['D'] = np.array(list(D_data.loc[D_data['delay_chip']==delay_chip,'D']))
+			self.D_calibration_data[delay_chip]['average delay (s)'] = np.array(list(D_data.loc[D_data['delay_chip']==delay_chip,'average delay (s)']))
+	
+	def set_delay(self, t: float):
+		if not (hasattr(self, 'time_to_D') and hasattr(self, 'time_to_FTUNE') and hasattr(self, 'FTUNE_to_time') and hasattr(self, 'D_calibration_data')):
+			raise RuntimeError(f'Prior to calling <set_delay> you must load calibration data using the method <load_calibration_files>. Otherwise you can manually set D and FTUNE using <set_D> and <set_FTUNE>.')
+		try:
+			t = float(t)
+		except:
+			raise ValueError(f'<delay> must be a float number specifying the delay in seconds. Received {delay} of type {type(delay)}.')
+		
+		chip_to_use = None
+		for delay_chip in self.time_to_D:
+			try:
+				D = int(np.ceil(self.time_to_D[delay_chip](t)))
+				chip_to_use = delay_chip
+			except ValueError as e:
+				continue
+		if chip_to_use is None:
+			raise ValueError(f'<t> is outside D calibration range. Received t = {t} s.')
+		# ~ D += 2 # This is to use a value of FTUNE that is "not too close to 0".
+		# ~ if D < 0:
+			# ~ D = 0
+		
+		fine_delay_to_remove = ((t - self.D_calibration_data[chip_to_use]['average delay (s)'][self.D_calibration_data[chip_to_use]['D']==D])**2)**.5
+		FTUNE_delay_at_0V = self.FTUNE_to_time[chip_to_use](0)
+		FTUNE_voltage = self.time_to_FTUNE[chip_to_use](FTUNE_delay_at_0V-fine_delay_to_remove)[0]
+		
+		for delay_chip in ['A','B']:
+			self.set_D(
+				delay_chip = delay_chip, 
+				D = D if delay_chip == chip_to_use else 0,
+			)
+			self.set_FTUNE(
+				delay_chip = delay_chip, 
+				FTUNE_V = FTUNE_voltage if delay_chip == chip_to_use else 0,
+			)
